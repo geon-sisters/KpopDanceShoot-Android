963ced8d29042f44558d8d93f5ff36309551ce72
geonhee7721 <geonhee7721@gmail.com>
2021-01-17 17:53:02 +0900

    Implement layout of search and bookmark view

---
+++ app/src/main/java/com/android/kpopdance/BookmarkFragment.kt
@@ -0,0 +1,88 @@
+package com.android.kpopdance
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.bookmark_fragment.*
+
+
+class BookmarkFragment : Fragment() {
+    companion object {
+        fun newInstance() = BookmarkFragment()
+    }
+
+    private lateinit var viewModel: BookmarkViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bookmark_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // for test
+        val youtubes = arrayListOf(
+            Youtube(
+                "noEhgQ0hI6M",
+                "[RAIN - Switch to me (duet with JWP)] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "jHW2yLQddNw",
+                "[TWICE - CRY FOR ME] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "scVCHsusm4c",
+                "[IZ*ONE - Sequence] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "noEhgQ0hI6M",
+                "[RAIN - Switch to me (duet with JWP)] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "jHW2yLQddNw",
+                "[TWICE - CRY FOR ME] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "scVCHsusm4c",
+                "[IZ*ONE - Sequence] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "noEhgQ0hI6M",
+                "[RAIN - Switch to me (duet with JWP)] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "jHW2yLQddNw",
+                "[TWICE - CRY FOR ME] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "noEhgQ0hI6M",
+                "[RAIN - Switch to me (duet with JWP)] dance practice mirrored",
+                "2021.01.16"
+            )
+        )
+
+        val bookmarkAdapter = YoutubeSmallAdapter()
+        bookmarkAdapter.youtubes = youtubes
+
+        bookmarkRecyclerView.adapter = bookmarkAdapter
+        bookmarkRecyclerView.layoutManager = LinearLayoutManager(this.context)
+
+        viewModel = ViewModelProviders.of(this).get(BookmarkViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/android/kpopdance/BookmarkViewModel.kt
@@ -0,0 +1,7 @@
+package com.android.kpopdance
+
+import androidx.lifecycle.ViewModel
+
+class BookmarkViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
