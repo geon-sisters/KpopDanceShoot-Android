78b3c6938f680f29da617ffbf7732ac6a526c276
GeonseoKim <waves72427721@gmail.com>
2021-01-19 23:00:09 +0900

    Implement Home Layout

--- app/src/main/java/com/android/kpopdance/HomeFragment.kt
+++ app/src/main/java/com/android/kpopdance/HomeFragment.kt
@@ -6,10 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.home_fragment.*
 
 
 class HomeFragment : Fragment() {
-
     companion object {
         fun newInstance() = HomeFragment()
     }
@@ -25,6 +26,62 @@ class HomeFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        // for test
+        val youtubes = arrayListOf(
+            Youtube(
+                "noEhgQ0hI6M",
+                "[RAIN - Switch to me (duet with JWP)] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "jHW2yLQddNw",
+                "[TWICE - CRY FOR ME] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "scVCHsusm4c",
+                "[IZ*ONE - Sequence] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "noEhgQ0hI6M",
+                "[RAIN - Switch to me (duet with JWP)] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "jHW2yLQddNw",
+                "[TWICE - CRY FOR ME] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "scVCHsusm4c",
+                "[IZ*ONE - Sequence] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "noEhgQ0hI6M",
+                "[RAIN - Switch to me (duet with JWP)] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "jHW2yLQddNw",
+                "[TWICE - CRY FOR ME] dance practice mirrored",
+                "2021.01.16"
+            ),
+            Youtube(
+                "noEhgQ0hI6M",
+                "[RAIN - Switch to me (duet with JWP)] dance practice mirrored",
+                "2021.01.16"
+            )
+        )
+
+        val youtubeBigAdapter = YoutubeBigAdapter()
+        youtubeBigAdapter.youtubes = youtubes
+
+        homeRecyclerView.adapter = youtubeBigAdapter
+        homeRecyclerView.layoutManager = LinearLayoutManager(this.context)
+
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         // TODO: Use the ViewModel
     }
---
+++ app/src/main/java/com/android/kpopdance/YoutubeBigAdapter.kt
@@ -0,0 +1,38 @@
+package com.android.kpopdance
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class YoutubeBigAdapter : RecyclerView.Adapter<YoutubeBigAdapter.ViewHolder>() {
+    var youtubes = mutableListOf<Youtube>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.youtube_big_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.bind(youtubes[position])
+    }
+
+    override fun getItemCount(): Int = youtubes.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val titleTextView = itemView.findViewById<TextView>(R.id.titleTextView)
+        private val dateTextView = itemView.findViewById<TextView>(R.id.dateTextView)
+        private val bigThumbnailView = itemView.findViewById<ImageView>(R.id.bigThumbnailView)
+
+        fun bind(youtube: Youtube) {
+            titleTextView.text = youtube.title
+            dateTextView.text = youtube.date
+            Glide.with(itemView)
+                    .load("https://img.youtube.com/vi/" + youtube.id + "/hqdefault.jpg")
+                    .into(bigThumbnailView)
+        }
+    }
+}
