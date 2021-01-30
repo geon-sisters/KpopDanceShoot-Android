0576b0be35c42af091bf107abc6ce8baed846add
geonhee7721 <geonhee7721@gmail.com>
2021-01-30 14:06:34 +0900

    Implement caching of youtubes

--- app/src/main/java/com/android/kpopdance/repository/YoutubeRepository.kt
+++ app/src/main/java/com/android/kpopdance/repository/YoutubeRepository.kt
@@ -1,11 +1,23 @@
 package com.android.kpopdance.repository
 
+import com.android.kpopdance.data.Youtube
 import com.android.kpopdance.model.YoutubeApi
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class YoutubeRepository(private val youtubeApi: YoutubeApi) {
-    var disposal = youtubeApi.get()
+    private var cachedYoutubes: List<Youtube> = arrayListOf()
+
+    var memory: Observable<List<Youtube>> = Observable.create { subscriber ->
+        subscriber.onNext(cachedYoutubes)
+        subscriber.onComplete()
+    }
+
+    var network: Observable<List<Youtube>> = youtubeApi.get()
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
+        .doOnNext {
+            cachedYoutubes = it
+        }
 }
--- app/src/main/java/com/android/kpopdance/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/android/kpopdance/viewmodel/HomeViewModel.kt
@@ -6,25 +6,36 @@ import androidx.lifecycle.MutableLiveData
 import com.android.kpopdance.contract.Contract.Companion.K_POP_DANCE
 import com.android.kpopdance.data.Youtube
 import com.android.kpopdance.repository.YoutubeRepository
+import io.reactivex.Observable
 
 class HomeViewModel(private val youtubeRepository: YoutubeRepository) : BaseViewModel() {
     private val TAG = K_POP_DANCE + HomeViewModel::class.simpleName
 
-    val youtubes: MutableLiveData<List<Youtube>> = MutableLiveData(arrayListOf())
+    private val _youtubes = MutableLiveData<List<Youtube>>(arrayListOf())
+    val youtubes: LiveData<List<Youtube>> get() = _youtubes
 
     private val _clickedYoutubeId = MutableLiveData<Event<String>>()
     val clickedYoutubeId: LiveData<Event<String>> get() = _clickedYoutubeId
 
     init {
         Log.i(TAG, "init")
-
-        addToDisposable(youtubeRepository.disposal
-            .subscribe({
-                youtubes.value = it
-                Log.d(TAG, "성공 : " + youtubes.value)
-            }, {
-                Log.d(TAG, "실패 : $it")
-            }))
+        addToDisposable(
+            Observable.concat(
+                youtubeRepository.memory,
+                youtubeRepository.network)
+                .filter {
+                    val isNotEmpty = it.isNotEmpty()
+                    Log.d(TAG, "cached: $isNotEmpty")
+                    isNotEmpty
+                }
+                .firstElement()
+                .subscribe({
+                    _youtubes.value = it
+                    Log.d(TAG, "Success")
+                }, {
+                    Log.d(TAG, "Fail : $it")
+                })
+        )
     }
 
     fun onYoutubeClicked(youtubeId: String) {
